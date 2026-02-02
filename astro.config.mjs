569687f8959adce32944027edb77f4c5ad99f016
K.Yamakawa <87867174+lCyou@users.noreply.github.com>
2026-02-02 23:52:49 +0900

    add: preact added

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,15 @@ import { remarkReadingTime } from './src/scripts/remark-reading-time.mjs';
 
 import icon from 'astro-icon';
 
+import preact from '@astrojs/preact';
+
 export default defineConfig({
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
 
   site: "https://lcyou.netlify.app",
-  integrations: [icon()],
+  integrations: [icon(), preact()],
 
   vite: {
     resolve: {
